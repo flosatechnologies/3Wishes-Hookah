9436f43906db1824a3c41cf1fec74706853b3f68
iamkupoe <samuelkupoe1@gmail.com>
2021-06-08 01:19:12 -0400

    Created add product screen

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/Dashboard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BsArchive, BsCreditCard } from "react-icons/bs";
@@ -7,67 +7,145 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { FaUserCircle } from "react-icons/fa";
 import logo from "../assets/images/logo.jpeg";
 import userImage from "../assets/images/contact.jpg";
+// import PaymentScreen from "./PaymentScreen";
+import AddProduct from "./AddProductScreen";
 
-function Dashboard() {
-  return (
-    <div className="container-fluid">
-      <div className="row menuBar">
-        <div className="col-lg-2">
-          <img src={require("../assets/images/logo.jpeg")} alt="logo" />
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonState: {
+        product: "inactive",
+        payment: "inactive",
+        delivery: "inactive",
+        logout: "inactive",
+      },
+    };
+  }
+
+  render() {
+    const handleRenderScreen = () => {
+      // if (this.state.buttonState.payment === "active")
+      if (this.state.buttonState.product === "active") {
+        return (
+          <div>
+            <AddProduct />
+          </div>
+        );
+      }
+    };
+    return (
+      <div className="container-fluid">
+        <div className="row menuBar">
+          <div className="col-lg-2">
+            <img src={logo} width="70" height="70" alt="logo" />
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-2 dashboardButtonSection">
-          <div className="row profileSection">
-            <div>
-              {/* <img
+        <div className="row">
+          <div className="col-lg-2 dashboardButtonSection">
+            <div className="row profileSection">
+              <div>
+                {/* <img
                 src={require("../assets/images/contact.jpg")}
                 alt="userImage"
-                width="100"
-                height="100"
+                width="70"
+                height="70"
               /> */}
-              <FaUserCircle
-                style={{
-                  width: 70,
-                  height: 70,
-                  marginRight: 20,
-                }}
-              />
+                <FaUserCircle
+                  style={{
+                    width: 70,
+                    height: 70,
+                    marginRight: 20,
+                  }}
+                />
+              </div>
+              <div>
+                <h5>Welcome,</h5>
+                <div>User</div>
+              </div>
             </div>
-            <div>
-              <h5>Welcome,</h5>
-              <div>User</div>
-            </div>
-          </div>
 
-          <div className="row">
-            <button className="dashboardButtons">
-              <BsArchive style={{ marginRight: 15 }} /> Product
-            </button>
+            <div className="row">
+              <button
+                onClick={() => {
+                  this.setState({
+                    buttonState: {
+                      product: "active",
+                      payment: "inactive",
+                      delivery: "inactive",
+                      logout: "inactive",
+                    },
+                  });
+                }}
+                id={this.state.buttonState.product}
+                className="dashboardButtons "
+              >
+                <BsArchive style={{ marginRight: 15 }} /> Product
+              </button>
+            </div>
+            <div className="row">
+              <button
+                onClick={() => {
+                  this.setState({
+                    buttonState: {
+                      product: "inactive",
+                      payment: "active",
+                      delivery: "inactive",
+                      logout: "inactive",
+                    },
+                  });
+                }}
+                id={this.state.buttonState.payment}
+                className="dashboardButtons"
+              >
+                <BsCreditCard style={{ marginRight: 15 }} />
+                Payment
+              </button>
+            </div>
+            <div className="row">
+              <button
+                onClick={() => {
+                  this.setState({
+                    buttonState: {
+                      product: "inactive",
+                      payment: "inactive",
+                      delivery: "active",
+                      logout: "inactive",
+                    },
+                  });
+                }}
+                id={this.state.buttonState.delivery}
+                className="dashboardButtons"
+              >
+                <RiTruckLine style={{ marginRight: 15 }} />
+                Delivery
+              </button>
+            </div>
+            <div className="row">
+              <button
+                onClick={() => {
+                  this.setState({
+                    buttonState: {
+                      product: "inactive",
+                      payment: "inactive",
+                      delivery: "inactive",
+                      logout: "active",
+                    },
+                  });
+                }}
+                id={this.state.buttonState.logout}
+                className="dashboardButtons"
+              >
+                <AiOutlineLogout style={{ marginRight: 15 }} />
+                Logout
+              </button>
+            </div>
           </div>
-          <div className="row">
-            <button className="dashboardButtons">
-              <BsCreditCard style={{ marginRight: 15 }} />
-              Payment
-            </button>
-          </div>
-          <div className="row">
-            <button className="dashboardButtons">
-              <RiTruckLine style={{ marginRight: 15 }} />
-              Delivery
-            </button>
-          </div>
-          <div className="row">
-            <button className="dashboardButtons">
-              <AiOutlineLogout style={{ marginRight: 15 }} />
-              Logout
-            </button>
-          </div>
+          <div className="col-lg-10 workspace">{handleRenderScreen()}</div>
         </div>
-        <div className="col-lg-10 workspace">{/* <h1>Work Space</h1> */}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Dashboard;
