4f09317fcfb33a4907cc409aba03d383bc35a30d
iamkupoe <samuelkupoe1@gmail.com>
2021-06-21 15:29:08 +0800

    Added Redux Store to the app

---
+++ src/Store/Reducers.js
@@ -0,0 +1,20 @@
+const initialState = {
+  users: "",
+};
+
+const allReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_USER":
+      const newUser = {
+        username: action.payload.username,
+        email: action.payload.email,
+        password: action.payload.password,
+      };
+      return { ...state, users: [...state.users, newUser] };
+
+    default:
+      return state;
+  }
+};
+
+export default allReducer;
