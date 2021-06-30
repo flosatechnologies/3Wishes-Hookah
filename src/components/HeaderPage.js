496ab0e3ea0762f7c90dfd2d9d159af7d2c7fbe8
iamkupoe <samuelkupoe1@gmail.com>
2021-06-30 20:02:32 +0800

    Added delivery info page

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -12,7 +12,7 @@ export class HeaderPage extends Component {
           {/* <Container> */}
           <LinkContainer to="/" exact={true}>
             <Navbar.Brand>
-              <img alt="" width="50" height="50" src={logo} />{" "}
+              <img alt="" width="50" height="50" src={logo} />
               <Navbar.Text className="text-light font-weight-bold">
                 3Wishes Hookah
               </Navbar.Text>
