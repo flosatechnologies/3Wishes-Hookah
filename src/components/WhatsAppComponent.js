d0f5783508e2c1e5e830e6f18330622586d352d3
iamkupoe <samuelkupoe1@gmail.com>
2021-11-05 16:57:40 -0500

    Footer and Whatsapp components updated

--- src/components/WhatsAppComponent.js
+++ src/components/WhatsAppComponent.js
@@ -5,7 +5,7 @@ import logo1 from "../assets/images/logo.png";
 export default function App() {
   return (
     <FloatingWhatsApp
-      phoneNumber="+233500205795"
+      phoneNumber="+233546835848"
       accountName="3wishes Support"
       avatar={logo1}
       statusMessage="At your Service"
